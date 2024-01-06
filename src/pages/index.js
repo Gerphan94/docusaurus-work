575db5d5888c7d0e8d3c33db84361e21ef66475d
Fan Duc'k <gerphan94@gmail.com>
2024-01-06 11:19:19 +0700

    1
    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Go to Documents 📖
           </Link>
         </div>
       </div>
