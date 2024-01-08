0ae7c9545221c56c7cbe6ccacadd2795367bb128
Fan Duc'k <gerphan94@gmail.com>
2024-01-08 08:16:20 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,27 +80,7 @@ const config = {
       footer: {
         style: 'dark',
         links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-             
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              
-            ],
-          },
+
         ],
       },
       prism: {
